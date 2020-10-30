6314b12e1c5cf9856842750334f4414c519fc78f
Alfrandhy <alfrandhy@gmail.com>
2020-10-30 16:24:34 +0700

    add frontend-material-laravel

--- resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('base')
-
-@section('content')
-    <p>Getting Started with Blade Template</p>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Route::get('/', function () {
-//     return view('welcome');
+//     return view('layouts.base');
 // });
 
 Route::get('/', 'BaseController@index');
@@ -24,3 +24,38 @@ Route::get('book', 'BookController@index');
 Route::get('book/{judul}', 'BookController@viewJudul');
 
 Route::get('book/view/{judul}', 'BookController@judulBuku');
+
+Route::get('/home', 'HomeController@index')->name('home');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+
+Route::group(['middleware' => 'auth'], function () {
+	Route::get('table-list', function () {
+		return view('pages.table_list');
+	})->name('table');
+
+	Route::get('typography', function () {
+		return view('pages.typography');
+	})->name('typography');
+
+	Route::get('icons', function () {
+		return view('pages.icons');
+	})->name('icons');
+
+	Route::get('map', function () {
+		return view('pages.map');
+	})->name('map');
+
+	Route::get('notifications', function () {
+		return view('pages.notifications');
+	})->name('notifications');
+});
+
+Route::group(['middleware' => 'auth'], function () {
+	Route::resource('user', 'UserController', ['except' => ['show']]);
+	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
+	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
+	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
+});
+
